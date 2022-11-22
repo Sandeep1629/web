f35d17c4be053c72db37c9dab4ae6b4777586ebf
Sandeep1629 <sandeepnaga763@gmail.com>
2022-11-22 11:01:17 +0530

    myproject

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+        hi mowabro , ede naa first website ra thnaks fro using mowa nenu mee sandeep
+      </h1>
+    </div>
+  );
+}
+
+export default App;
